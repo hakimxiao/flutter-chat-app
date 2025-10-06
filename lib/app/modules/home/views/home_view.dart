c176e8d9804e22c0d70a534e000ea06c787d3670
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-06 21:01:08 +0700

    - IMPLEMENT LAWAN CHAT BY REALTIME IN DB -
    * Membuat pembacaan user yang terkoneksi dengan kita pada home view akan tampil berdasarkan yang ada di db dan juga.
    * Bersifat realtime dimana jika kita hapus dari db maka akan include didalam homeview
    
    !!! Kita memanfaatkan fitur ** stream **  Digunakan untuk membaca sebuag data secara realtime
    
    cara penggunaan nya sederhana :
     1. Buat instance dan juga    lokasi yang akan di stream pada controller.
     2. Bangun UI nya dengan stream builder dengan value stream nya diisi stream yang kita declare di dalam controller
    
     ** stream dan stream builder itu butuh type agar bisa mengakses value spesifik seperti data()
    
     - pada stream kita bisa ambil type nya dengan menghover snapshot ambil di yang pertama.
     - pada streambuilder kita bisa dengan menghover snapshot juga yang ada di parameter. yang pertama juga
    
     berikut contoh : stream dan streambuilder type :
    1.  Stream<DocumentSnapshot<Map<String, dynamic>>>
    
     2. AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> || ambil Type nya saja

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: avoid_print
+
+import 'package:chatapp/app/controllers/auth_controller.dart';
 import 'package:chatapp/app/routes/app_pages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,26 +12,7 @@ import '../controllers/home_controller.dart';
 class HomeView extends GetView<HomeController> {
   HomeView({super.key});
 
-  final List<Widget> myChats = List.generate(
-    20,
-    (index) => ListTile(
-      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.black26,
-        child: Image.asset('assets/logo/noimage.png', fit: BoxFit.cover),
-      ),
-      title: Text(
-        'Orang ke-${index + 1}',
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-      ),
-      subtitle: Text(
-        'Status Orang ke-${index + 1}',
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-      ),
-      trailing: Chip(label: Text("3")),
-    ),
-  ).reversed.toList();
+  final authC = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
@@ -67,10 +52,42 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: myChats.length,
-              itemBuilder: (context, index) => myChats[index],
+            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: controller.chatsStream(authC.user.value.email!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  var allChats = (snapshot.data!.data()
+                      as Map<String, dynamic>)["chats"] as List;
+                  print(snapshot.data!.data());
+                  return ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: allChats.length,
+                    itemBuilder: (context, index) => ListTile(
+                      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.black26,
+                        child: Image.asset('assets/logo/noimage.png',
+                            fit: BoxFit.cover),
+                      ),
+                      title: Text(
+                        'Orang ke-${index + 1}',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                      subtitle: Text(
+                        'Status Orang ke-${index + 1}',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      trailing: Chip(label: Text("3")),
+                    ),
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
           ),
         ],
